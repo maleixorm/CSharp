f8b093fdc52759f8c05604d2f9a5cb6712be0b57
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-08-09 14:13:00 -0300

    Tipos e Funções no C#

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,54 @@
+﻿// Screen Sound
+string mensagemDeBoasVindas = "Seja bem-vindo ao Screen Sound!";
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu ()
+{
+    Console.WriteLine("\n1 - Registrar Banda");
+    Console.WriteLine("2 - Listar todas as Bandas");
+    Console.WriteLine("3 - Avaliar Banda");
+    Console.WriteLine("4 - Media de avaliação da Banda");
+    Console.WriteLine("9 - Sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 2:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 9:
+            Console.WriteLine("Tchau tchau :)");
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+}
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
